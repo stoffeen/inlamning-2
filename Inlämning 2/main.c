e839a174d76f1d106040826ef7c54d875a6bb631
stoffeen <christoffer.olsson@student.kyh.se>
2022-10-09 16:21:54 +0200

    final commit (probably)

--- Inlämning 2/main.c
+++ Inlämning 2/main.c
@@ -1,45 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
 
-int main(void)
+typedef struct
+{
+    char *name;
+    char *value;
+}Currency;
+
+
+int main()
+
 {
     FILE *infile;
     char *buffer;
-    long numbytes;
+    long num_bytes = 10000;
+    int i;
+
+
+
+    // Declaring how big the currency list is
+    Currency currencies_list[100];
+
 
     // Open file for reading
     infile = fopen ("currencies.txt", "r");
 
-    // Quick if file dosn't exist
+
+    // Quit if file doesn't exist
     if(infile == NULL)
+    {
+        printf("Error while opening file %s", infile);
         return 1;
+    }
 
-    // Get number of bytes
+
+    // Get the number of bytes and setting the pointer at the last byte
     fseek(infile, 0L, SEEK_END);
-    numbytes = ftell(infile);
+    num_bytes = ftell(infile);
 
-    // Reset the files position indicator to the beginning of the file
+
+    // Reset the files pointer to the beginning of the file
     fseek(infile, 0L, SEEK_SET);
 
+
     // Grab memory for the buffer to hold text
-    buffer = (char*)calloc(numbytes, sizeof(char));
+    buffer = (char*)calloc(num_bytes, sizeof(char));
+
 
     // Memory error
     if(buffer == NULL)
             return 1;
 
+
     // Copy all the text into buffer
-    fread(buffer, sizeof(char), numbytes, infile);
+    fread(buffer, sizeof(char), num_bytes, infile);
     fclose(infile);
 
-    // Confirm that we have read the file by writing to console
-    printf("Currencies\n---------------\n\n%s\n---------------", buffer);
+
+    printf("Text from buffer: \n-----------------\n");
+    printf("%s%s\n", buffer, "\n------------------------------------");
+
+
+    int next_curr = 0;
+    char *buffer_text = strtok(buffer, " \t\n");
+
+
+    // Add words to currencies list as long it's not NULL
+    while (buffer_text != NULL)
+    {
+        currencies_list[next_curr].name = buffer_text;
+        buffer_text = strtok(NULL, " \t\n ");
+        currencies_list[next_curr].value = buffer_text;
+        buffer_text = strtok(NULL, " \t\n");
+        next_curr++;
+    }
+
+
+
+    // Get the length of structlist
+    int structlength = sizeof(currencies_list)/sizeof(currencies_list[0]);
+
+
+    for (i = 0; i <= structlength; i++)
+    {
+        if(currencies_list[i].name != NULL)
+            printf("Currency: %s\t%s%s\t%s\n", currencies_list[i].name,"Value: ", currencies_list[i].value, "ore");
+        else
+        {
+            break;
+        }
+    }
+
 
     // Free the memory used for buffer
     free(buffer);
 
     return 0;
 }
-
